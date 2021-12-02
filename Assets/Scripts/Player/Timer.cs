520a1583cf511b29204633217f0b95639a0df3ad
Des_LOAD <desloadwork@gmail.com>
2021-12-02 21:11:42 +0700

    [TM add]Fonts + Timer text

--- Assets/Scripts/Player/Timer.cs
+++ Assets/Scripts/Player/Timer.cs
@@ -2,26 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float time=60;
+    public float time = 60;
     public GameObject canvasTimer;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         time -= Time.deltaTime;
-        canvasTimer.GetComponent<Text>().text = time.ToString("f0");
+        canvasTimer.GetComponent<TextMeshProUGUI>().text = time.ToString("f0");
         if (time <= 0)
         {
             //Поражение
         }
 
-        }
+    }
 }
