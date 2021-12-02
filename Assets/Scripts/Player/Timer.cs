b7c9e98f138488778f38d3f2c5fe759c3a63fa1e
FatFat <dima.stolyarov2013@yandex.ru>
2021-12-02 20:51:20 +0700

    Timer
    таймер

---
+++ Assets/Scripts/Player/Timer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float time=60;
+    public GameObject canvasTimer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        canvasTimer.GetComponent<Text>().text = time.ToString("f0");
+        if (time <= 0)
+        {
+            //Поражение
+        }
+
+        }
+}
